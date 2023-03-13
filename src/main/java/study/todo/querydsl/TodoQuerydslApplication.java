cd1ca0c4218159596dbdcb2a36b4e9aaad84aa6a
LIM GWANG MIN <rhdl142@danawa.com>
2023-03-14 08:55:12 +0900

    Querydsl 학습

---
+++ src/main/java/study/todo/querydsl/TodoQuerydslApplication.java
@@ -0,0 +1,13 @@
+package study.todo.querydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class TodoQuerydslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoQuerydslApplication.class, args);
+	}
+}
